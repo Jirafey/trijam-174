2d9a4479760e02f43197cbe244e6af8d5114d0b4
N0thing13 <99495809+N0thing13@users.noreply.github.com>
2022-06-17 13:55:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
+
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
